3a72c7f3950995c050ec5f8878e6befcd7077e34
nju33 <nju33.ki@gmail.com>
2018-06-21 02:26:25 +0900

    wip

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,4 +1,6 @@
-// import WebSocket from 'ws';
+import WebSocket from 'isomorphic-ws';
+
+// window.__ = _WebSocket;
 
 
 // //example of using a message handler from the inject scripts
@@ -20,14 +22,18 @@ chrome.storage.onChanged.addListener(changes => {
   const ws = new WebSocket(`ws://localhost:${changes.portNumber.newValue}`);
   ws.onopen = function () {
     console.log(arguments);
+    return;
   }
   ws.onmessage = function () {
     console.log(arguments);
+    return;
   };
   ws.onclose = function () {
     console.log(arguments);
+    return;
   };
   ws.onerror = function () {
     console.log(arguments);
+    return;
   };
 })
